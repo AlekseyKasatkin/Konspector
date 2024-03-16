261b59574d12637dc8f07df3aece82249ea090e7
Maxim Lastin <lastinm@mail.ru>
2024-03-17 01:09:48 +0300

    Добавлена вторая версия файла для streamlit, позволяющая легко импортировать ее функции. Добавлен тест для функции run_file в файле sl_2.py.

---
+++ sl_test.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import sys
+import os
+sys.path.append(os.path.dirname(__file__))
+from sl_2 import run_file
+
+# run_file теперь будет функцией main
+#run_file = main()
+
+def test_run_file(monkeypatch):
+    test_file_name = "sl_test.py"
+    test_file_path = os.path.join(os.path.dirname(__file__), test_file_name)
+
+    # Создаем контекст для виртуальной сессии Streamlit
+    with st.empty():
+        # Заменяем функцию set_option, чтобы устанавливать параметры перед запуском
+        monkeypatch.setattr(st, 'set_option', lambda key, value: None)
+
+        # Загружаем и запускаем файл в Streamlit, используя функцию main
+        result = run_file(test_file_path)
+
+    # Убедимся, что результат выполнения файла соответствует ожидаемому
+    assert "Время выполнения скрипта" in result, f"Ожидаемая подстрока 'Время выполнения скрипта' не найдена в строке '{result}'"
+
