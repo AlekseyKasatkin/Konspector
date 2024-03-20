f98345724cfcf581756e8282a7779d915bd521f7
Maxim Lastin <lastinm@mail.ru>
2024-03-20 22:30:34 +0300

    Добавлена проверка существования файла. Если файл mp3 не найден, то возвращается None.

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,7 @@ import glob
 
 def getfile(path):
     files = glob.glob(f'{path}/*.mp3')
-    return files[0]
-
-
+    if len(files) > 0:   # Проверяем, что список files не пустой
+        return files[0]  # Возвращаем первый элемент списка
+    else:
+        return None      # Возвращаем None, если список пустой
